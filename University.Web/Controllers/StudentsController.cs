c0d7dab82b2bda5f58e66db4f64f898ac789a533
BrayanMotta <brayan.motta14@gmail.com>
2021-08-11 20:20:13 -0500

    Agregar archivos de proyecto.

---
+++ University.Web/Controllers/StudentsController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using University.Web.Models;
+using System.Collections.Generic;
+
+namespace University.Web.Controllers
+{
+    public class StudentsController : Controller
+    {
+        [HttpGet]
+        // GET: Students
+        public ActionResult Index()
+        {
+
+            var students = new List<Student>(); //ilimitada
+
+            students.Add(new Student { ID = 1, FirstMidName = "David", LastName = "Santafe", EnrollmentDate = DateTime.Now });
+            students.Add(new Student { ID = 2, FirstMidName = "Carson", LastName = "Alexander", EnrollmentDate = DateTime.Now });
+            students.Add(new Student { ID = 3, FirstMidName = "Meredith", LastName = "Alonso", EnrollmentDate = DateTime.Now });
+
+            ViewBag.Data = "Mesaje de prueba";
+            ViewBag.Message = "Mesaje de prueba";
+
+           // ViewData["Data"] = "Mensaje de prueba";
+
+            return View(students);
+        }
+
+        [HttpGet]
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Student student)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                    return View(student);
+
+                if (student.EnrollmentDate > DateTime.Now)
+                    throw new Exception("La fecha de matricula no puede ser mayor a la fecha actual");
+
+                var id = student.ID;
+                var FirstMidName = student.FirstMidName;
+                var LastName = student.LastName;
+                var enrollmentDate = student.EnrollmentDate;
+            }
+            catch (Exception ex)
+            {
+
+                ModelState.AddModelError(string.Empty, ex.Message);
+            }
+
+            return View(student);
+  
+        }
+    }
+}
---
+++ University.Web/Models/Student.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace University.Web.Models
+{
+    
+    public class Student
+    {
+        [Display(Name = "ID")]
+        [Required (ErrorMessage ="El campo ID es obligatorio")]
+        public int ID { get; set; }
+
+        [Display(Name = "Last Name")]
+        [Required(ErrorMessage = "El campo Last Name es obligatorio")]
+        [StringLength(50, ErrorMessage ="Maximo 50 caracteres")]
+        public string LastName { get; set; }
+
+        [Display(Name = "First MidName")]
+        [Required(ErrorMessage = "El campo First MidName es obligatorio")]
+        [StringLength(50, ErrorMessage = "Maximo 50 caracteres")]
+        public string FirstMidName { get; set; }
+
+        [Display(Name = "Enrollment Date")]
+        [Required(ErrorMessage = "El campo Enrollment Date es obligatorio")]
+        [DataType(DataType.DateTime)]
+        public DateTime EnrollmentDate { get; set; }
+    }
+}
